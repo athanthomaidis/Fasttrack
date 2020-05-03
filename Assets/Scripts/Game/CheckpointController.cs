978af6bc87adedfea9d3c71cf533a69037cbbe72
Christos Louarasis <clouarasis1@sheffield.ac.uk>
2020-05-03 16:07:57 +0300

    sdfghnbv

--- Assets/Scripts/Game/CheckpointController.cs
+++ Assets/Scripts/Game/CheckpointController.cs
@@ -6,11 +6,12 @@ public class CheckpointController : MonoBehaviour
 {
 	//public Transform checkpoint;
 	public GameObject player;
+	public GameObject aiCar;
 	public int checkpointCounter;
 	// Use this for initialization
 	void Start()
 	{
-        Debug.Log(player.gameObject.tag);
+        
         checkpointCounter = 0;
 		//player = GameObject.FindWithTag("Player");
         //Debug.Log(player.gameObject.tag);
@@ -22,12 +23,14 @@ public class CheckpointController : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter(Collider plyr)
 	{
-        checkpointCounter++;
-        Debug.Log("inside");
-		if (plyr.tag.Equals("Player"))
+        if(plyr.tag.Equals(aiCar.tag))
+        {
+            Debug.Log("inside AI");
+            checkpointCounter++;
+        }
+		if (plyr.tag.Equals(player.tag))
 		{
-            Debug.Log("inside if");
-
+            Debug.Log("inside PLAYER");
             checkpointCounter++;
 		}
 	}
