e2203572a138f21d9bc6fd4e6804844aa5a37e70
Christos Louarasis <clouarasis1@sheffield.ac.uk>
2020-05-03 19:58:08 +0300

    sdfgh

---
+++ Assets/Scripts/InstantiationExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiationExample : MonoBehaviour
+{
+    public GameObject myPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
